94c265e71d6297c879b359a7a99ce9ae9e784e2e
erka <erik.westerberg@hotmail.se>
2019-08-08 18:14:11 +0200

    creates a new specfile, wrong branch

--- src/Modules/Auth.js
+++ src/Modules/Auth.js
@@ -1,36 +1,41 @@
-import axios from 'axios'
+import axios from "axios";
 
-
-const apiUrl = 'http://localhost:3000/api/v1';
+const apiUrl = "http://localhost:3000/api/v1";
 
 const authenticate = async (email, password) => {
-  const path = apiUrl + '/auth/sign_in';
+  const path = apiUrl + "/auth/sign_in";
   try {
-    let response = await axios.post(path, { email: email, password: password })
-    await storeAuthCredentials(response)
-    sessionStorage.setItem('current_user', JSON.stringify({ id: response.data.data.id }));
-    return { authenticated: true }
+    let response = await axios.post(path, { email: email, password: password });
+    await storeAuthCredentials(response);
+    sessionStorage.setItem(
+      "current_user",
+      JSON.stringify({ id: response.data.data.id })
+    );
+    return { authenticated: true };
   } catch (error) {
-    return { authenticated: false, message: error.response.data.errors[0] }
+    return { authenticated: false, message: error.response.data.errors[0] };
   }
 };
 
 const storeAuthCredentials = ({ data, headers }) => {
-  return new Promise((resolve) => {
-    const uid = headers['uid'],
-      client = headers['client'],
-      accessToken = headers['access-token'],
-      expiry = headers['expiry'];
+  return new Promise(resolve => {
+    const uid = headers["uid"],
+      client = headers["client"],
+      accessToken = headers["access-token"],
+      expiry = headers["expiry"];
 
-    sessionStorage.setItem('credentials', JSON.stringify({
-      uid: uid,
-      client: client,
-      access_token: accessToken,
-      expiry: expiry,
-      token_type: 'Bearer'
-    }));
-    resolve(true)
-  })
+    sessionStorage.setItem(
+      "credentials",
+      JSON.stringify({
+        uid: uid,
+        client: client,
+        access_token: accessToken,
+        expiry: expiry,
+        token_type: "Bearer"
+      })
+    );
+    resolve(true);
+  });
 };
 
-export { authenticate, storeAuthCredentials }
+export { authenticate, storeAuthCredentials };
