2e67d85397d715124baf4aec42a9e11159224b11
erka <erik.westerberg@hotmail.se>
2019-08-08 11:20:21 +0200

    Recafocters with a own inputfield component and render it to app.js, all green

--- src/App.js
+++ src/App.js
@@ -1,40 +1,27 @@
-import React, { Component } from 'react';
-import DisplayCooperResult from './Components/DisplayCooperResult';
+import React, { Component } from "react";
+import DisplayCooperResult from "./Components/DisplayCooperResult";
+import InputFields from "./Components/InputFields";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      distance: '',
-      gender: 'female',
-      age: ''
-    }
+      distance: "",
+      gender: "female",
+      age: ""
+    };
   }
 
   onChange(event) {
     this.setState({
       [event.target.id]: event.target.value
-    })
+    });
   }
 
- render() {
+  render() {
     return (
       <div>
-        <div>
-          <label>Distance</label>
-          <input id="distance" onChange={this.onChange.bind(this)}></input>
-        </div>
-
-        <select id="gender" onChange={this.onChange.bind(this)}>
-          <option value="female">Female</option>
-          <option value="male">Male</option>
-        </select>
-
-        <div>
-          <label>Age</label>
-          <input id="age" onChange={this.onChange.bind(this)}></input>
-        </div>
-
+        <InputFields inputChangeHandler={this.onChange.bind(this)} />
         <DisplayCooperResult
           distance={this.state.distance}
           gender={this.state.gender}
