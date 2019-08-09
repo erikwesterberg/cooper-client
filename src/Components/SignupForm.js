607c6aa1494195b31fd0cbc853687f5c58b2ecd1
erka <erik.westerberg@hotmail.se>
2019-08-09 14:55:49 +0200

    styles the button and header

--- src/Components/SignupForm.js
+++ src/Components/SignupForm.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const SignupForm = (props) => {
   return (
-    <form id="signUp-form" className="input-data-form">
+    <form id="signUp-form" className="">
       <div>
         <label >Email</label>
         <input id="email" onChange={props.inputChangeHandler}></input>
