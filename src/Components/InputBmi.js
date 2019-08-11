ee3a3b90bd1e34867ac2907c4c298f7f34c1204f
erka <erik.westerberg@hotmail.se>
2019-08-11 14:49:22 +0200

    added options between metric and imperial on bmi calc

--- src/Components/InputBmi.js
+++ src/Components/InputBmi.js
@@ -1,27 +1,31 @@
-import React from 'react';
+import React from "react";
 
-const InputBmi = (props) => {
+const InputBmi = props => {
   return (
     <>
-          <div className="text">
+      <div className="input-data-form">
+        <div className="text">
           <label>Weight</label>
-          <input
-            className="input-field"
-            name="weight"
-          
-          />
+          <input className="input-field" name="weight" />
+
+          <select>
+            <option className="options" value="metric">
+              Metric
+            </option>
+            <option className="options" value="imperial">
+              Imperial
+            </option>
+          </select>
         </div>
 
         <div className="text">
           <label>Height</label>
-          <input
-            className="input-field"
-            name="height"
-          />
-          <button onClick={(e) => props.hideBMI(e)} >Hide</button>
+          <input className="input-field" name="height" />
+          <button onClick={e => props.hideBMI(e)}>Hide</button>
         </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
 export default InputBmi;
